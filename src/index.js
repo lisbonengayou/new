f4cdddedc0e1027cccb15e53e9f74daf409baf79
lisbone Ngayou <lisbonengayou@gmail.com>
2021-03-25 19:10:21 +0200

    homework

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Создайте страницу, которая спрашивает имя у пользователя и выводит его.
+let name = prompt('enter your name', 'lisbone');
+alert(name)
+ 
+//Исправьте сложение
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+
+alert(Number(a)+ Number(b));
