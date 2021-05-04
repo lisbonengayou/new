dd60af95693fcdd278094956ff12a685d1ed38d7
lisbone Ngayou <lisbonengayou@gmail.com>
2021-05-04 10:04:50 +0300

    add new files to track

--- src/index.js
+++ src/index.js
@@ -1,53 +1,47 @@
-//Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+/*У вас есть массив объектов user, и в каждом из них есть 
+user.name. Напишите код, 
+который преобразует их в массив имён.
+user = [
+  { name: "John", age: 25 },
+  { name: "Pete", age: 30 },
+  { name: "Mary", age: 28 },
+]
 
-//Функция должна быть нечувствительна к регистру:
- 
-function checkSpam(str) {
-    
-  lowerStr = str.toLowerCase();
-  if(lowerStr.includes("viagra") || lowerStr.includes("xxx") ){
-    return true
-}else{
-  return false
-}
-}
+let newuser =  user.map(obj=> obj.name )
+console.log(newuser)
 
-console.log(checkSpam( "you are viagra boys"))
-console.log(checkSpam("you are a xxx girl"));
-console.log(checkSpam("smart boy"))
-console.log(checkSpam("VIAgra girl"))
-console.log(checkSpam("XXx at midnight"))
-console.log(checkSpam("go far away"))
+У вас есть массив объектов user, и у 
+каждого из объектов есть name, surname и id.
+
+Напишите код, который создаст ещё один массив объектов с
+ параметрами id и fullName, где fullName – состоит из name 
+ и surname.
+
+ user2 =[
+{ name: "John", surname: "Smith", id: 1 },
+{ name: "Pete", surname: "Hunt", id: 2 },
+{ name: "Mary", surname: "Key", id: 3 },
+]
+
+ let user2map = user2.map( user =>({
+        fullName: `${user.name} ${user.surname}`,
+        id: user.id  
+ }));
   
-//Создайте функцию truncate(str, maxlength), которая проверяет длину строки str и, если она превосходит maxlength, заменяет конец str на "…", так, чтобы её длина стала равна maxlength.
-
-//Результатом функции должна быть та же строка, если усечение не требуется, либо, если необходимо, усечённая строка
-
-function truncate(str, maxlength){
-  str;
-  maxlength;
-  if(str.length >= maxlength){
-    return str.substring(0,maxlength) + "...";
-  }else{
-    return str;
-  }
-
-};
-console.log(truncate("i am the woman of your dream",20))
-console.log(truncate("i am your futur!",20))
+     console.log(user2map)
+     console.log(user2map[2].fullName)*/
 
 
-//Создайте функцию extractCurrencyValue(str), которая будет из такой строки выделять числовое значение и возвращать его.
+     //let test= prompt("gess the number",);
 
+    // if (test > 0){
+    //     console.log(1);
+    // } else if (test < 0) {
+     //   console.log(-1);
+     //}else{  
+      //   console.log(0);
+     //}
 
-function extractCurrencyValue(str){
-  str;
-
-  return +str.substr(1,10);
-}
- console.log(extractCurrencyValue('$120'))
- console.log(extractCurrencyValue('$120') === 120)
-
-
-
+      
  
+
